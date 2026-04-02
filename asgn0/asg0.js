6dba80a4dae206cd7bee32b16c2ec8de1018134c
PJ-004 <pranavjworkacc123@gmail.com>
2026-04-01 22:46:31 -0700

    Drew a red line onto a black canvas

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -1,7 +1,7 @@
 // DrawTriangle.js (c) 2012 matsuda
 function main() {  
   // Retrieve <canvas> element
-  var canvas = document.getElementById('example');  
+  var canvas = document.getElementById('canvas');  
   if (!canvas) { 
     console.log('Failed to retrieve the <canvas> element');
     return false; 
@@ -10,7 +10,22 @@ function main() {
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  // Draw a black rectangle
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';                   // Set color to black
+  ctx.fillRect(0, 0, canvas.width, canvas.height);        // Fill a rectangle with the color
+
+  handleDrawEvent();
+}
+
+function handleDrawEvent() {
+  let center_x = canvas.width / 2;
+  let center_y = canvas.height / 2;
+  var ctx = canvas.getContext('2d');
+
+  ctx.strokeStyle = 'red';
+
+  ctx.beginPath();
+  ctx.moveTo(center_x, center_y);
+  ctx.lineTo(center_x + 75, center_y + 50);
+  ctx.stroke();
 }
